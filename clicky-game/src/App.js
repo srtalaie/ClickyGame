2c2bf84ab0340584b5a4e28db94b86fe49b84e3b
srtalaie <srt4721@yahoo.com>
2018-09-25 18:32:17 -0700

    Made images.json

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PicCard from './components/PicCard/PicCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Clicky Game</h1>
         </header>
-        <div className="jumbotron">
-          <h3>Click on Each Image to Get Points</h3>
-          <h4>But don't click on any one more than once!</h4>
-        </div>
         <div id="score"></div>
-        <div id="image-holder">`</div>
+        <div id="image-holder">
+          <PicCard />
+        </div>
       </div>
     );
   }
