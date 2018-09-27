5bd5e216225288dba7e4a97fe6019dd5c4c1bb75
srtalaie <srt4721@yahoo.com>
2018-09-27 09:26:37 -0700

    Got it to shuffle

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PicCard from './components/PicCard/PicCard';
+import PicCard from './components/PicCard';
+import Wrapper from './components/Wrapper/Wrapper';
+import Images from './images.json';
+
+function shuffleArray(array){
+  for(let i = array.length - 1; i > 0; i--){
+    let j = Math.floor(Math.random() * i);
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+} 
 
 class App extends Component {
+
+  state = {
+    Images
+  }
+
+  componentDidMount(){
+    this.setState(shuffleArray(this.state.Images))
+  }
+
+  isClicked = event => {
+    //Look at library activity in class-repo
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,7 +38,14 @@ class App extends Component {
         </header>
         <div id="score"></div>
         <div id="image-holder">
-          <PicCard />
+          <Wrapper>
+          {this.state.Images.map(image =>(
+            <PicCard 
+              imageUrl = {image.image}
+              key = {image.id}
+            />
+          ))}
+          </Wrapper>
         </div>
       </div>
     );
--- clicky-game/src/components/PicCard/PicCard.js
+++ clicky-game/src/components/PicCard/PicCard.js
@@ -1,52 +1,8 @@
-import React, { Component } from 'react';
-import Images from '../../images.json';
+import React from 'react';
+import './PicCard.css'
 
-class PicCard extends Component{
-    state = {
-        // clicked: false,
-        imageUrl: "",
-        imageArray: [Images[0].image, Images[1].image, Images[2].image, Images[3].image, Images[4].image, Images[5].image, Images[6].image, Images[7].image, Images[8].image, Images[9].image, Images[10].image, Images[11].image, Images[12].image]
-    }
-
-    // componentDidMount() {
-    //     this.setState.imageUrl = '../../assets/House_Arryn.png';
-    // }
-
-    randomHouseImage = () => {
-        let ranNum = Math.floor(Math.random() * (this.state.imageArray.length + 1));
-        this.setState.imageUrl = this.state.imageArray[ranNum];
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="row">
-                    <img onClick={this.randomHouseImage} src={this.state.imageArray[0]} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                </div>
-                <div className="row">
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                </div>
-                <div className="row">
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                </div>
-                <div className="row">
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                    <img onClick={this.randomHouseImage} src={this.state.imageUrl} className="img-fluid" alt="House-Banner"></img>
-                </div>
-            </div>
-        );
-    }
-}
+const PicCard = props => (
+        <img className="img-fluid" src={props.imageUrl} alt="House-Banner" key={props.id} height="200px" width="200px" onClick={() => props.isClicked(props.id)} clicked={props.clicked}></img>
+    );
 
 export default PicCard;
